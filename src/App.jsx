5d15095346d27977de309ce67a05e53810c2839c
Sierra <UDES074@mycwt.com>
2023-05-12 10:21:20 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+// import QRCode from 'react-qr-code'
+import gru1 from './utils/img/gru1.jpg'
+import gru2 from './utils/img/gru2.jpg'
+import whatsappLogo from './utils/img/Digital_Glyph_Green.png'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      {/* <QRCode value="https://drive.google.com/file/d/1BJftfhAqJ1Kcs9gEal2HAyVrJ6XSR9BG/view?usp=share_link"/>
+      <h2>Gru station!</h2> */}
+      <div className='img-menu'>
+        <img src={gru1} alt=""/>
+        <img src={gru2} alt=""/>
+      </div>
+      <img src="https://scontent-sjc3-1.xx.fbcdn.net/v/t39.8562-6/…KQ7Oqv7NzTNWkrjfqVhk-pKqNPUji1lyhtsPQ&oe=6463A50F" alt=""/>
+      <div className='img-wa'>
+        <a href="https://wa.me/573246803306">
+          <b>Pide aqui!</b>
+          <img src={whatsappLogo} alt="" />
+        </a>
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default App
